3dc8a65e8aa27ae44b94d6d8af64b22dbabbb394
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-04-22 13:27:52 +0300

    RONNIE MANHATTAN TRAINING Variables and assignment and mtc operators

---
+++ ronnie_training.py
@@ -0,0 +1,93 @@
+
+# create in 1990s by Guido Van Russom
+# started in Xmass season, with urge of creating a lnsguage as powerfull as C but easyto write
+# he wanted to simplify C, and easy as B created  a laguange AB which is part of C and B and this language has no name
+# he was watching a commedy clip, he was watching its montey Py and then named his language AB "PYTHON"
+# A python file is called a script
+# Python is dynamically typed ie you don't need to specify variables
+# Python is ...
+
+# Python Syntax & Semantics 
+# Syntax the rules that govern the use of a language eg in eng I did went (gramatically wrong) an empthatic verb with past tense
+# Semantics the meaning of the rules of a language, most times the semantics can be right but the meaning wrong or VV
+# 
+# First semantic of python
+# Telling a computer to reserve memory space
+
+
+
+# Variables, Statement, Datatypes
+
+# age = 15 # python variable
+
+# # 1salary = 15
+# # $country = 'uganda'     # not a python variable string variable
+
+# mySalary = 5000    # camel casing
+# my_salary = 9000   # using an underscore bttn word
+
+
+# Adding two numbers
+a = 17  # declaring a variable 'a' and assigning a value 17
+b = 19
+c = a + b
+print("The sum is ", c)
+
+# # Make a simple python program printing out of your name and address where you stay
+
+
+# Cobine different variables as one
+height = 200
+weight = 65
+name = "Manhattan"
+
+# print(name, "You are", height,"cm and", weight,"kg")
+
+
+num1, num2, num3 = 10,90,20
+sum = num1+num2+num3
+print(sum)
+
+mynumber = yourNumber = ourNumber = 50
+print("My number is",mynumber)   # 50
+print("Your number is", yourNumber) # 50
+print("Our number is", ourNumber)  # 50
+
+
+# Operators
+number1 = 10
+number1 += 15
+print(number1)
+
+# Comparison operator
+num1 = 90
+num2 = 55
+print(num1<num2)
+print(num1>=num2)
+print(num1<num2)
+
+
+# Make another python program with details about you like height, weight and BMI
+
+# Try to get to know about
+# Arithmetic Operators or types in Python
+'''
+1. Mathematical Operators ie + - * /
+2. floor division //
+3. Exponential operator ** ie num1 ** num2
+4. Modulus (%) a remender after a division, a small value % big value you remain with small value eg 5%10 is 5
+'''
+
+
+myName = "Manhattan"
+
+
+#  Variables in python
+# num1 = 10
+# num2 = 20
+
+# num3 = num1 + num2
+
+# print(type(num3))
+
+# my
